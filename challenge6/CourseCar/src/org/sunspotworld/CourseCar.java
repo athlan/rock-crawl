4fc283d64a3dccc0651786fb75831bc81e3fbbf1
Ben Duong <bduong@vmware.com>
2012-11-19 18:13:17 -0500

    added Light turning and backup

--- challenge6/CourseCar/src/org/sunspotworld/CourseCar.java
+++ challenge6/CourseCar/src/org/sunspotworld/CourseCar.java
@@ -8,6 +8,7 @@ package org.sunspotworld;
 import com.sun.spot.peripheral.radio.RadioFactory;
 import com.sun.spot.resources.Resources;
 import com.sun.spot.resources.transducers.IAnalogInput;
+import com.sun.spot.resources.transducers.ILightSensor;
 import com.sun.spot.resources.transducers.ISwitch;
 import com.sun.spot.resources.transducers.ITriColorLED;
 import com.sun.spot.resources.transducers.ITriColorLEDArray;
@@ -33,17 +34,22 @@ public class CourseCar extends MIDlet {
     private static final int SERVO_CENTER_VALUE = 1500;
     private static final int FB_SERVO_HIGH = 200;
     private static final int LR_SERVO_HIGH = 500;
+    private static final int LIGHT_STEP = 200;
+    private int turningPeriod = 3000;
+    private int lightIndication = 0;         //ranges from 0 - 740
+    private int environLight = 100;
+    private int baselineRight = 0;
+    private int baselineLeft = 0;
     private EDemoBoard eDemo = EDemoBoard.getInstance();
     private IAnalogInput rightSensor = eDemo.getAnalogInputs()[EDemoBoard.A0];
     private IAnalogInput leftSensor = eDemo.getAnalogInputs()[EDemoBoard.A1];
     private IAnalogInput frontSensor = eDemo.getAnalogInputs()[EDemoBoard.A2];
-    private int baselineRight = 0;
-    private int baselineLeft = 0;
     private ISwitch sw1 = (ISwitch) Resources.lookup(ISwitch.class, "SW1");
     private ISwitch sw2 = (ISwitch) Resources.lookup(ISwitch.class, "SW2");
     private Servo frontBackServo = new Servo(eDemo.getOutputPins()[EDemoBoard.H0]);
     private Servo leftRightServo = new Servo(eDemo.getOutputPins()[EDemoBoard.H1]);
     private ITriColorLEDArray leds = (ITriColorLEDArray) Resources.lookup(ITriColorLEDArray.class);
+    private ILightSensor light = eDemo.getLightSensor();
 
     protected void startApp() throws MIDletStateChangeException {
         System.out.println("Hello, world");
@@ -55,17 +61,21 @@ public class CourseCar extends MIDlet {
         ITriColorLED led = leds.getLED(0);
         while (true) {
             led.setRGB(100, 0, 0);                    // set color to moderate red
-            while (sw2.isOpen()) {
+            while (sw1.isOpen()) {
                 blinkLed(led);
                 stop();// wait 1 second
+                goStraight();
+                System.out.println("Ready");
             }
             recordBaseline();
             led.setRGB(0, 0, 100);
             led.setOn();
-            while (sw1.isOpen()) { //Start when you press the switch
+            while (sw2.isOpen()) { //Start when you press the switch
 
                 if (checkCorner()) {
                     turnCorner();
+                } else if (getFrontValue() < 17) {
+                    backUp();
                 } else {
                     System.out.println(baselineLeft + " " + getLeftValue());
                     if (getLeftValue() < baselineLeft - 10) {
@@ -77,6 +87,7 @@ public class CourseCar extends MIDlet {
                     }
 
                 }
+                System.out.println("Rollin Out");
                 goForward();
                 blinkLed(led);
             }
@@ -87,11 +98,62 @@ public class CourseCar extends MIDlet {
         // This is not currently called by the Squawk VM
     }
 
+    private void backUp() {
+        System.out.println("B-b-b-back it up");
+        stop();
+        Utils.sleep(250);
+        System.out.println("Turn values" + getLeftValue() + " " + getRightValue());
+        if(getLeftValue() < getRightValue() - 15) {
+            turnLeft();
+        } else if (getRightValue() < getLeftValue() -15) {
+            turnRight();
+        } else {
+            goStraight();
+        }
+        Utils.sleep(500);
+        long time = System.currentTimeMillis() + turningPeriod/2;
+
+        while (System.currentTimeMillis() < time) {
+            goBackward();
+            Utils.sleep(250);
+        }
+        stop();
+        goStraight();
+        Utils.sleep(250);
+    }
+
     private boolean checkCorner() {
-        return false;
+        try {
+            lightIndication = light.getValue();         //ranges from 0 - 740
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        if (lightIndication > environLight + LIGHT_STEP) {
+
+            leds.getLED(5).setRGB(100, 0, 0);
+            leds.getLED(5).setOn();
+            return true;
+        } else {
+            leds.getLED(5).setOff();
+            return false;
+        }
     }
 
     private void turnCorner() {
+        System.out.println("Ermahgerd Light! Turnin nowh");
+        stop();
+        Utils.sleep(500);
+        turnLeft();
+        Utils.sleep(1000);
+
+        long time = System.currentTimeMillis() + turningPeriod;
+
+        while (System.currentTimeMillis() < time) {
+            goForward();
+            Utils.sleep(250);
+        }
+        goStraight();
     }
 
     private void turnLeft() {
@@ -107,9 +169,13 @@ public class CourseCar extends MIDlet {
     }
 
     private void goForward() {
-        System.out.println("Forward");
+        //System.out.println("Forward");
         frontBackServo.setValue(SERVO_CENTER_VALUE - FB_SERVO_HIGH);
+    }
 
+    private void goBackward() {
+        //System.out.println("Backward");
+        frontBackServo.setValue(SERVO_CENTER_VALUE + FB_SERVO_HIGH);
     }
 
     private void stop() {
@@ -121,37 +187,48 @@ public class CourseCar extends MIDlet {
         }
     }
 
+    private int getFrontValue() {
+        double value = 0;
+        for (int ii = 0; ii < 5; ii++) {
+            try {
+                double volts = frontSensor.getVoltage();
+                value += (18.67 / (volts + 0.167));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                return 10000;
+            }
+        }
+        return (int) (value/5);
+        
+    }
+
     private int getLeftValue() {
 
         double value = 0;
         for (int ii = 0; ii < 5; ii++) {
             try {
-                value +=(leftSensor.getVoltage() / .009766 * 2.4);
+                value += (leftSensor.getVoltage() / .009766 * 2.4);
             } catch (IOException ex) {
                 ex.printStackTrace();
                 return -1;
             }
         }
 
-        return (int) (value/5);
+        return (int) (value / 5);
     }
 
     private int getRightValue() {
         double value = 0;
         for (int ii = 0; ii < 5; ii++) {
             try {
-                value +=(rightSensor.getVoltage() / .009766 * 2.4);
+                value += (rightSensor.getVoltage() / .009766 * 2.4);
             } catch (IOException ex) {
                 ex.printStackTrace();
                 return -1;
             }
         }
 
-        return (int) (value/5);
-    }
-
-    private int getFrontValue() {
-        return 50;
+        return (int) (value / 5);
     }
 
     private void recordBaseline() {
@@ -164,6 +241,11 @@ public class CourseCar extends MIDlet {
         if (baselineRight < 0) {
             baselineRight = 0;
         }
+    }
+
+    public void getEnvironLight() throws IOException {
+        environLight = light.getAverageValue(25);
+
     }
 
     /**
